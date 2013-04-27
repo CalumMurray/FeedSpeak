d7c5317e2136fa8421541b780129a89cca65b2a8
Neil Fordyce <N.Fordyce@dundee.ac.uk>
2013-04-28 00:53:05 +0100

    trying out some twitter oauth stuff

--- src/main/java/com/hackeurope/feedspeak/AuthorizeServlet.java
+++ src/main/java/com/hackeurope/feedspeak/AuthorizeServlet.java
@@ -4,13 +4,23 @@
  */
 package com.hackeurope.feedspeak;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  *
@@ -18,6 +28,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "AuthorizeServlet", urlPatterns = {"/auth"})
 public class AuthorizeServlet extends HttpServlet {
+
     /**
      * Handles the HTTP
      * <code>GET</code> method.
@@ -30,7 +41,45 @@ public class AuthorizeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        try {
+            /*            ConfigurationBuilder cb = new ConfigurationBuilder();
+             cb.setDebugEnabled(true)
+             .setOAuthConsumerKey("aTR1FAEsR0hAj9w47ko9Tg")
+             .setOAuthConsumerSecret("XDSn1TTobWDBy46gZAfm6ya2kYkmli30B2vD2ixxpMA");
 
+             TwitterFactory tf = new TwitterFactory(cb.build());*/
+
+            Twitter twitter = TwitterFactory.getSingleton();
+            twitter.setOAuthConsumer("aTR1FAEsR0hAj9w47ko9Tg", "XDSn1TTobWDBy46gZAfm6ya2kYkmli30B2vD2ixxpMA");
+
+            RequestToken requestToken = twitter.getOAuthRequestToken();
+
+            AccessToken accessToken = null;
+            
+            response.sendRedirect(requestToken.getAuthorizationURL());
+            //request.getRequestDispatcher(requestToken.getAuthorizationURL()).forward(request, response);
+            
+            /*while (null == accessToken) {
+                
+
+                System.out.println("Open the following URL and grant access to your account:");
+                System.out.println(requestToken.getAuthorizationURL());
+                System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
+
+                try {
+                        accessToken = twitter.getOAuthAccessToken();
+                } catch (TwitterException te) {
+                    if (401 == te.getStatusCode()) {
+                        System.out.println("Unable to get the access token.");
+                    } else {
+                        te.printStackTrace();
+                    }
+                }
+            }*/
+
+        } catch (TwitterException ex) {
+            Logger.getLogger(AuthorizeServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -45,6 +94,10 @@ public class AuthorizeServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+    }
+
+    private static void storeAccessToken(int useId, AccessToken accessToken) {
+        System.out.println(accessToken.getToken());
+        System.out.println(accessToken.getTokenSecret());
     }
 }
