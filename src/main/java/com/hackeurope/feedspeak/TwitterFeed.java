08d7a20d4695651c192bab6c773ca7f35ae1ddd2
Calum Murray <Cal_92@hotmail.co.uk>
2013-04-27 22:33:18 +0100

    Added User bean model.  Commented out some unused code.

--- src/main/java/com/hackeurope/feedspeak/TwitterFeed.java
+++ src/main/java/com/hackeurope/feedspeak/TwitterFeed.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.hackeurope.feedspeak;
 
 import java.io.IOException;
@@ -18,8 +15,6 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.User;
 import twitter4j.conf.ConfigurationBuilder;
-import twitter4j.*;
-import twitter4j.api.*;
 
 /**
  *
---
+++ src/main/java/com/hackeurope/feedspeak/User.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hackeurope.feedspeak;
+
+/**
+ *
+ * @author Calum
+ */
+public class User {
+    
+}
